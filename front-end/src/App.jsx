c475b0cd42dff86a8a3b1cd0c1fcdb31b301e3b5
kazakhpunk <nursultansagyntay.1@gmail.com>
2025-10-26 13:48:00 -0400

    chore: added routing

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -1,7 +1,14 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/dashboard";
+
 
 function App() {
-	return <></>;
+	return (
+		<Routes>
+			<Route path="/dashboard" element={<Dashboard />} />
+		</Routes>
+	);
 }
 
 export default App;
