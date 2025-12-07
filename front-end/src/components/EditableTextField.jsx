abf41e5e5d5fe320f5557eceea76299c2a3a14d5
zs-5 <177980470+zs-5@users.noreply.github.com>
2025-12-07 05:46:52 -0500

    feat: editable text fields

---
+++ front-end/src/components/EditableTextField.jsx
@@ -0,0 +1,62 @@
+import { useId, useRef, useState } from "react";
+import { Check, LoaderCircle, Pencil, X } from "lucide-react";
+import "./EditableTextField.css";
+
+export default function EditableTextField({ value, placeholder, autosuggestList, onEdit }) {
+	const [isEditing, setIsEditing] = useState(false);
+	const [isLoading, setIsLoading] = useState(false);
+	const inputRef = useRef();
+	const autosuggestListId = useId();
+
+	return (
+		<div className="editable-field" onClick={() => setIsEditing(true)}>
+			{!isEditing ? (
+				<>
+					{value}
+					<Pencil className="edit-icon" size="20" color="#666" />
+				</>
+			) : (
+				<>
+					<input
+						type="text"
+						defaultValue={value}
+						placeholder={placeholder}
+						list={autosuggestListId}
+						disabled={isLoading}
+					/>
+
+					{autosuggestList && (
+						<datalist id={autosuggestListId}>
+							{autosuggestList.map((value) => (
+								<option key={value} value={value}></option>
+							))}
+						</datalist>
+					)}
+
+					<button
+						disabled={isLoading}
+						className="edit-field-discard"
+						onClick={(evt) => {
+							evt.stopPropagation();
+							setIsEditing(false);
+						}}
+					>
+						<X color={isLoading ? undefined : "#f12f55"} />
+					</button>
+
+					<button
+						className="edit-field-save"
+						onClick={(evt) => {
+							evt.stopPropagation();
+							if (inputRef.current.value.length === 0) return alert("Please enter something, it cannot be blank");
+							setIsEditing(false);
+							onEdit(inputRef.current.value);
+						}}
+					>
+						{isLoading ? <LoaderCircle className="spin-loader" color="#07a287" /> : <Check color="#07a287" />}
+					</button>
+				</>
+			)}
+		</div>
+	);
+}
