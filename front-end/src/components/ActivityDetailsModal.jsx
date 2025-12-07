0ed4b6444d5d540dd3fd7a8752a11d206d7856fe
zs-5 <177980470+zs-5@users.noreply.github.com>
2025-12-07 05:47:12 -0500

    feat: activity details & editing modal (not complete)

---
+++ front-end/src/components/ActivityDetailsModal.jsx
@@ -0,0 +1,135 @@
+import { useEffect, useState } from "react";
+import { Heart, LoaderCircle } from "lucide-react";
+import EditableTextField from "./EditableTextField";
+import "./ActivityDetailsModal.css";
+
+export default function ActivityDetailsModal({ groupId, activities, selectedActivity, onUpdate, ref }) {
+	const [isLoading, setIsLoading] = useState([]);
+
+	const userId = JSON.parse(atob(localStorage.getItem("JWT").split(".")[1])).id;
+	let categories = activities.map((activity) => activity.category);
+	categories = categories.filter(
+		(category, index) => categories.map((c) => c.toLowerCase()).indexOf(category.toLowerCase()) === index,
+	);
+
+	useEffect(() => {
+		setTimeout(() => ref.current.scrollTo(0, 0), 10); // Needed because modal fully scrolled, likely due to (unintentional) autofocusing on the checkbox
+		console.log("scrolled");
+	}, [ref, selectedActivity]);
+
+	return (
+		<dialog className="activity-details-popup" ref={ref} closedby="any">
+			{/* Name */}
+			<section>
+				{/* <h2 className="activity-name"></h2> */}
+				<h3>Name</h3>
+				<EditableTextField
+					value={selectedActivity.name}
+					placeholder="Activity name"
+					onEdit={(newVal) => {
+						console.log("New val:", newVal); // Z-TEMP
+					}}
+				/>
+			</section>
+
+			{/* Images. Note: Just a stub, will need to implement carousel/gallery, and adding/removing pictures UI */}
+			<img
+				width="300"
+				height="150"
+				src={selectedActivity.images?.at(0) ? selectedActivity.images[0] : "https://placehold.co/300x150"}
+			/>
+
+			{/* Category */}
+			<section>
+				<h3>Category</h3>
+
+				<EditableTextField
+					value={selectedActivity.category}
+					placeholder="Category"
+					autosuggestList={categories}
+					onEdit={(newVal) => {
+						console.log("New val:", newVal); // Z-TEMP
+					}}
+				/>
+			</section>
+
+			{/* Tags */}
+			<section>
+				<h3>Tags</h3>
+
+				<div className="card-tags">
+					{selectedActivity.tags?.map((tag) => (
+						<span key={tag} className="tag">
+							#{tag}
+						</span>
+					))}
+				</div>
+			</section>
+
+			{/* Likes */}
+			<section className="members-section">
+				<h3>Likes ({selectedActivity.likes?.length})</h3>
+
+				<ul className="members-list">
+					{selectedActivity.likes
+						?.toSorted((user1, user2) => (user1._id === userId ? -1 : user2._id === userId ? 1 : 0))
+						.map((user) => (
+							<li key={user._id}>
+								<img src={user.profilePicture || "https://placehold.co/48"} />
+								<span>{user._id === userId ? "You" : user.username}</span>
+								<Heart color="red" fill="red" />
+							</li>
+						))}
+				</ul>
+			</section>
+
+			{/* Location. Unsure about implementing rn, as we should probably switch to a string instead of coordinates to avoid having to deal with maps, etc */}
+			{/* <section>
+					<h3>Location</h3>
+
+					{selectedActivity.location?.coordinates.map((coor) => coor.toFixed(2)).join(", ")}
+				</section> */}
+
+			{/* Done */}
+			<section>
+				{/* <h3>Completed</h3> */}
+
+				<label className="activity-completed">
+					{isLoading.includes("done") ? (
+						<LoaderCircle size="32px" color="#666" className="spin-loader" />
+					) : (
+						<input
+							type="checkbox"
+							checked={selectedActivity.done}
+							onChange={async (evt) => {
+								setIsLoading([...isLoading, "done"]);
+
+								// await new Promise((r) => setTimeout(r, 2000));
+								const res = await fetch(
+									`${import.meta.env.VITE_BACKEND_ORIGIN}/api/groups/${groupId}/activities/${selectedActivity._id}`,
+									{
+										headers: {
+											Authorization: `Bearer ${localStorage.getItem("JWT")}`,
+											"Content-Type": "application/json",
+										},
+										method: "PATCH",
+										body: JSON.stringify({ done: evt.target.checked }),
+									},
+								);
+
+								if (!res.ok) return alert("Failed to mark activity as done");
+								else onUpdate(await res.json());
+
+								setIsLoading(isLoading.filter((x) => x !== "done"));
+							}}
+						/>
+					)}
+					<div>
+						<p>Completed?</p>
+						<p>Save it in your memory book!</p>
+					</div>
+				</label>
+			</section>
+		</dialog>
+	);
+}
