fc22941eb9137cce3dffb190129607d369346378
zs-5 <177980470+zs-5@users.noreply.github.com>
2025-12-09 16:17:00 -0500

    feat: remove unneeded dotenv module

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -1,15 +1,12 @@
 import app from "./app.js";
 import { connectDB } from "./db.js";
 
-// Load environment variables
-import "dotenv/config";
-
 const PORT = process.env.PORT || 8000;
 
 const startServer = async () => {
 	await connectDB();
 	app.listen(PORT, () => {
-    console.log(`Express server running at http://localhost:${PORT}`);
+		console.log(`Express server running at http://localhost:${PORT}`);
 	});
 };
 
